3a5818fcb8c28813061d1d604d4c99a7831e4290
lepouletsuisse <xxpoulletxx@gmail.com>
2017-01-19 16:26:34 +0100

    Update config

--- config/config.js
+++ config/config.js
@@ -1,19 +1,18 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
-var mongoURI = process.env.MANGODBURI || "192.168.99.100";
-var labName = "TWEB2";
-var collection = labName + "";
+var mongoURI = process.env.MANGODB_URI || "192.168.99.100";
+var collection = "TeacherAsked";
 
 var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'tweb-labo2-dev'
+      name: collection + '-dev'
     },
     port: process.env.PORT || 3000,
-    collection: labName + '-dev',
-    db: 'mongodb://' + mongoURI + '/' + labName + '-dev',
+    collection: collection + '-dev',
+    db: 'mongodb://' + mongoURI + '/' + collection + '-dev',
     socketIoURL: process.env.SOCKETIOURL || 'http://localhost:3000',
     jwtsecret: process.env.JWTSECRET ||'supersecretsharedkey'
 },
@@ -21,11 +20,11 @@ var config = {
   test: {
     root: rootPath,
     app: {
-      name: 'tweb-labo2-test'
+      name: collection + '-test'
     },
     port: process.env.PORT || 3000,
-    collection: labName + '-test',
-    db: 'mongodb://' + mongoURI + '/' + labName + '-test',
+    collection: collection + '-test',
+    db: 'mongodb://' + mongoURI + '/' + collection + '-test',
     socketIoURL: process.env.SOCKETIOURL || 'http://localhost:3000',
     jwtsecret: process.env.JWTSECRET ||'supersecretsharedkey'
   },
@@ -33,11 +32,11 @@ var config = {
   production: {
     root: rootPath,
     app: {
-      name: 'tweb-labo2-prod'
+      name: collection + '-prod'
     },
     port: process.env.PORT || 3000,
-    collection: labName + '-prod',
-    db: 'mongodb://' + mongoURI + '/' + labName + '-prod',
+    collection: collection + '-prod',
+    db: 'mongodb://' + mongoURI + '/' + collection + '-prod',
     socketIoURL: process.env.SOCKETIOURL || 'http://localhost:3000',
     jwtsecret: process.env.JWTSECRET ||'supersecretsharedkey'
   }
